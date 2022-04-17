235cff68d1cdc4ed91367f64651b4e5faeca83ed
Douglas Maia <78104102+DouglasMai4@users.noreply.github.com>
2022-04-17 19:42:31 -0300

    Botões melhores ajustados, Adicionado mais tabelas

--- Mysql.py
+++ Mysql.py
@@ -2,8 +2,8 @@ import PySimpleGUI as sg
 import mysql.connector as mysql
 
 sg.theme('DarkGrey11')
-layout = [[sg.Text('Usuario'), sg.InputText()],
-          [sg.Text('Senha  '), sg.InputText(password_char='*')],
+layout = [[sg.Text('Usuario:', size=(8, 1)), sg.InputText(expand_x=True)],
+          [sg.Text('Senha:', size=(8, 1)), sg.InputText(password_char='*', expand_x=True)],
           [sg.Button('Cancelar'), sg.Button('OK')]]
 window = sg.Window('Conectar', layout)
 event, values = window.read()
@@ -13,30 +13,39 @@ if event == sg.WIN_CLOSED or event == 'Cancelar':
     exit
 elif event == 'OK':
     mydb = mysql.connect(
-      host='144.22.239.20',
-      port='3307',
+      host='168.138.140.85',
+      port='3306',
       user=values[0],
       password=values[1],
       database='PRODUTOS'
     )
     mycursor = mydb.cursor()
     while True:
-        sql = "SELECT Nome, ValorAnt, ValorAtu FROM PRODUTO"
+        sql = "SELECT * FROM PRODUTO"
         mycursor.execute(sql)
         myresult = mycursor.fetchall()
 
         itens = []
-        data = []
         c = 0
         for row in myresult:
-            data.append([])
-            data[c].append(row[0])
-            data[c].append(row[1])
-            data[c].append(row[2])
+            itens.append([])
+            itens[c].append(row[0])
+            itens[c].append(row[1])
+            itens[c].append(row[2])
+            itens[c].append(row[3])
+            itens[c].append(row[4])
+            itens[c].append(row[5])
+            itens[c].append(row[6])
+            itens[c].append(row[7])
+            itens[c].append(row[8])
+            itens[c].append(row[9])
+            itens[c].append(row[10])
+            itens[c].append(row[11])
+            itens[c].append(row[12])
             c += 1
 
-        cabecario = ['Nome', 'Valor Anterior', 'Valor Atual']
-        layout = [[sg.Table(values=data, headings=cabecario, max_col_width=25,
+        cabecario = ['Nome', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+        layout = [[sg.Table(values=itens, headings=cabecario, max_col_width=25,
                             background_color='#313641',
                             auto_size_columns=True,
                             display_row_numbers=True,
@@ -45,8 +54,8 @@ elif event == 'OK':
                             alternating_row_color='#1c1f22',
                             key='-TABLE-',
                             row_height=20)],
-                  [sg.Button('Adicionar', size=(21, 1)), sg.Button('Remover', size=(20, 1))],
-                  [sg.Button('Cancelar', size=(47, 1))]]
+                  [sg.Button('Adicionar', expand_x=True), sg.Button('Remover', expand_x=True)],
+                  [sg.Button('Cancelar', expand_x=True)]]
         window = sg.Window('Menu', layout)
         event, values = window.read()
         window.close()
@@ -56,9 +65,19 @@ elif event == 'OK':
         elif event == 'Adicionar':
             while True:
                 layout = [[sg.Text('Adicionar Item')],
-                          [sg.Text('Nome:           '), sg.InputText()],
-                          [sg.Text('Valor Anterior:'), sg.InputText()],
-                          [sg.Text('Valor Atual:    '), sg.InputText()],
+                          [sg.Text('Nome:', size=(9, 1)), sg.InputText(expand_x=True)],
+                          [sg.Text('Janeiro:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Fevereiro:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Março:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Abril:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Maio:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Junho:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Julho:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Agosto:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Setembro:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Outubro:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Novembro:', size=(9, 1)), sg.InputText(0, expand_x=True)],
+                          [sg.Text('Dezembro:', size=(9, 1)), sg.InputText(0, expand_x=True)],
                           [sg.Button('Cancelar'), sg.Button('OK')]]
                 window = sg.Window('Adicionar', layout)
                 event, values = window.read()
@@ -67,21 +86,21 @@ elif event == 'OK':
                     break
                 elif event == 'OK':
                     mycursor.execute(
-                        f'INSERT INTO PRODUTO (Nome, ValorAnt, ValorAtu) VALUES ("{values[0]}", {values[1]}, {values[2]});')
+                        f'INSERT INTO PRODUTO VALUES ("{values[0]}", {values[1]}, {values[2]}, {values[3]}, {values[4]}, {values[5]}, {values[6]}, {values[7]}, {values[8]}, {values[9]}, {values[10]}, {values[11]}, {values[12]});')
                     mydb.commit()
                     window.close()
 
                     layout = [[sg.Text('Item adicionado com sucesso')],
-                              [sg.Button('OK', size=(22, 1))]]
+                              [sg.Button('OK', expand_x=True)]]
                     window = sg.Window('Sucesso', layout)
                     event, values = window.read()
                     if event == sg.WIN_CLOSED or event == 'OK':
                         window.close()
         elif event == 'Remover':
             while True:
-                layout = [[sg.Text('Nome do item: '), sg.InputText()],
-                          [sg.Button('Remover Item', size=(26, 1)), sg.Button('Remover Todos', size=(25, 1))],
-                          [sg.Button('Cancelar', size=(57, 1))]]
+                layout = [[sg.Text('Nome do item: '), sg.InputText(expand_x=True)],
+                          [sg.Button('Remover Item', expand_x=True), sg.Button('Remover Todos', expand_x=True)],
+                          [sg.Button('Cancelar', expand_x=True)]]
                 window = sg.Window('Remover Item', layout)
                 event, values = window.read()
                 if event == sg.WIN_CLOSED or event == 'Cancelar':
@@ -93,7 +112,7 @@ elif event == 'OK':
                     window.close()
 
                     layout = [[sg.Text('Item removido com sucesso')],
-                              [sg.Button('OK', size=(22, 1))]]
+                              [sg.Button('OK', expand_x=True)]]
                     window = sg.Window('Sucesso', layout)
                     event, values = window.read()
                     if event == sg.WIN_CLOSED or event == 'OK':
